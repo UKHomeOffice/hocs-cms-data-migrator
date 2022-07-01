f2b62158cde7adb65c70f72469141ee41fba2c8b
Stuart Whitlam <stuart.whitlam1@digital.homeoffice.gov.uk>
2022-07-01 07:57:15 +0100

    fix localstack health check

--- config/localstack/1-setup-sqs.sh
+++ config/localstack/1-setup-sqs.sh
@@ -6,7 +6,7 @@ export AWS_SECRET_ACCESS_KEY=UNSET
 export AWS_DEFAULT_REGION=eu-west-2
 
 ## make sure that localstack is running in the pipeline
-until curl http://localstack:4566/health --silent | grep -q "running"; do
+until curl http://localstack:4566/health --silent | grep -q "\"sqs\": \"available\""; do
    sleep 5
    echo "Waiting for LocalStack to be ready..."
 done
